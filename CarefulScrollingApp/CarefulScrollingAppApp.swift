85e12329f6a879774de23ba969ee9a356ca9bb4b
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-01 10:23:04 +0300

    Initial Commit

---
+++ CarefulScrollingApp/CarefulScrollingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulScrollingAppApp.swift
+//  CarefulScrollingApp
+//
+//  Created by jopootrivatel on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarefulScrollingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
